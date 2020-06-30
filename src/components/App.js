b64ce2f668818b2bef6cea6b1c60a83800bc3b23
Nabendu <nabendu.biswas@gmail.com>
2020-06-30 09:51:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
 import ImageList from './imageList';
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 class App extends React.Component  {
     state = { images: [] };
@@ -10,7 +11,7 @@ class App extends React.Component  {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term},
             headers: {
-                Authorization: 'Client-ID b205b07b2bc8eab3c685fb4beb577a2f9d7d81aee9f50635305d040123a6b708'
+                Authorization: `Client-ID ${API_KEY}`
             }
         })
 
